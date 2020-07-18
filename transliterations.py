bb522d9f8280a7395dc15e79077a9f492f4583fe
joseprzmoreno <jos.pe.mo@gmail.com>
2020-07-18 22:42:07 +0200

    Adds romanization of Chinese (pinyin) via Python library.

--- transliterations.py
+++ transliterations.py
@@ -1,7 +1,8 @@
 from transliterate import translit
+import pinyin
 
 def has_transliteration(tat_code):
-    codes = ["hye","ell","kat","rus"]
+    codes = ["hye","ell","kat","rus","cmn"]
     if tat_code in codes:
         return True
     return False
@@ -19,8 +20,16 @@ def get_translit_lang_code(tat_code):
             return codes[1][count]
         count+=1
 
+def get_pinyin(txt):
+    txt2 = ""
+    for character in txt:
+        txt2 += character + " "
+    return pinyin.get(txt2)
+
 def get_transliteration(tat_lang, txt):
     if tat_lang in ["hye","ell","kat","rus"]:
         tr_lang = get_translit_lang_code(tat_lang)
         return translit(txt, tr_lang, reversed=True)
+    elif tat_lang == "cmn":
+        return get_pinyin(txt)
     return ""
